0a958e7a25a66ad2b07f05e74cdb8c3a90b1d1c2
fakelog <fakelog@users.noreply.github.com>
2025-04-03 18:52:18 +0500

    rules: make RegexResult is pub

--- src/rules/message/mod.rs
+++ src/rules/message/mod.rs
@@ -6,7 +6,7 @@ use crate::types::PayloadItem;
 use async_trait::async_trait;
 
 pub use command::CommandRule;
-pub use regex::RegexRule;
+pub use regex::{RegexResult, RegexRule};
 pub use text::TextRule;
 
 #[async_trait]
