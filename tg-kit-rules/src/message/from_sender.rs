df37e8990853c565a2e9858b0152508ca3ffd34a
fakelog <48557356+fakelog@users.noreply.github.com>
2025-04-23 21:26:05 +0000

    tg-kit-rules: add FromSenderRule

---
+++ tg-kit-rules/src/message/from_sender.rs
@@ -0,0 +1,25 @@
+use async_trait::async_trait;
+use grammers_client::types::Message;
+use tg_kit::{rules::MessageRule, types::PayloadItem};
+
+pub struct FromSenderRule {
+    id: i64,
+}
+
+impl FromSenderRule {
+    pub fn new(id: i64) -> Self {
+        Self { id }
+    }
+}
+
+#[async_trait]
+impl MessageRule for FromSenderRule {
+    async fn matches(&self, message: &Message) -> PayloadItem {
+        let sender = match message.sender() {
+            Some(sender) => sender,
+            None => return Box::new(false) as PayloadItem,
+        };
+
+        Box::new(sender.id() == self.id) as PayloadItem
+    }
+}
--- tg-kit-rules/src/message/mod.rs
+++ tg-kit-rules/src/message/mod.rs
@@ -1,9 +1,11 @@
 mod command;
+mod from_sender;
 mod or;
 mod regex;
 mod text;
 
 pub use command::CommandRule;
+pub use from_sender::FromSenderRule;
 pub use or::OrRule;
 pub use regex::*;
 pub use text::TextRule;
